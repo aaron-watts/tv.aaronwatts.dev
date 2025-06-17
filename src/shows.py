ca3f1efd1c06a0a1a09ed74cbd3edeba07b19064
Aaron Watts <aaron.watts@murena.io>
2025-06-17 18:41:24 +0100

    add new shows

--- src/shows.py
+++ src/shows.py
@@ -4,4 +4,16 @@ shows = [
     '62220', # Rick and Morty: The Anime
     '55936', # Rick and Morty: The Non- Canonical Adventures
     '70005', # Lazarus
+    '37196', # Invincible
+    '15299', # The Boys
+    '62998', # Gen V
+    '72198', # Carol and the End of the World
+    '53474', # Fired on Mars
+    '40329', # Love, Death and Robots
+    '2993', # Stranger Things
+    '4201', # One Punch Man
+    '47182', # Smiling Friends
+    '538', # Futurama
+    '28757', # Big Mouth
+    '69327', # Common Side Effects
 ]
